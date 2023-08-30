fce377bb2770f8737ebd7c7c50dd80bb60d873a3
Haleh Bozorgnia <bozorgnia2010@gmail.com>
2023-08-30 15:14:18 -0700

    landscaper: user can buy battery-powered lawnmower

--- landscaper.py
+++ landscaper.py
@@ -1,76 +1,118 @@
 game_data = {
     "money": 0,
     "scissors_counter": 0,
-    "lawnmower_counter":0,
+    "lawnmower_counter": 0,
     "battery_lawnmower_counter":0,
     "quit":False
 }
 while(True):
     user_input= int(input("""
                 What would you like to do?
-                [1] Using my teeth
+                [1] Using my teeth and earn $5
                 [2] Buy a pair of rusty scissors for $5
-                [3] Use my rusty scissors
+                [3] Use my rusty scissors and earn $5
                 [4] buy old-timey push lawnmower for $25
-                [5] fancy battery-powered lawnmower for $250
-                [6] Use  my battery-powered
-                [7] Quit the Game
+                [5] Use my old lawnmower and earn $50
+                [6] Buy fancy battery-powered lawnmower for $250
+                [7] Use  my battery-powered and earn $100
+                [8] Quit the Game
                 """))
     if (user_input == 1):
         game_data["money"]+=1
         print(f"You have ${game_data['money']} in your account")
-       
+########################################   
+##Option 2
+########################################
     if(user_input == 2):
-        if(game_data["money"]>=5 and game_data["scissors_counter"]<1 ):
-            print(f"You have now ${game_data['money']} in your account before purchase.")
-            game_data["money"]= game_data["money"] -5 
-            game_data["scissors_counter"]+=1
-            print(f"You have now ${game_data['money']} in your account after purchase.")
-        elif(game_data["scissors_counter"]==1):
-            print("You cab buy pair of rusty scissors one time")
+        if(game_data["money"]>=5):
+            if(game_data["scissors_counter"] < 1):
+                game_data["money"]= game_data["money"]- 5
+                game_data["scissors_counter"]= 1
+                print(f"Now You have ${game_data['scissors_counter']}scissors")
+                print(f"Congratulations, Now You have ${game_data['money']}in your account")
+            else:
+                print(f"Now You have one pair of rusty scissors.")
         else:
-            print("You do not have enought money to buy pair of rusty scissors.")
-
+            print("You do not have enough money")
+########################################   
+##Option 3
+########################################
     if(user_input == 3):
         if(game_data["scissors_counter"]==1):
           game_data["money"]+=5
-          print(f"You have now ${game_data['money']} in your account before purchase.") 
+          print(f"Congratulations,You have now ${game_data['money']} in your account before purchase.") 
         else:
-            print("You do not have rusty scissors, select option[2] to buy the scissors ")
-
+          print("You do not have rusty scissors, select option[2] to buy the scissors ")
+########################################   
+##Option 4
+########################################
+    
     if(user_input == 4):
-        if(game_data["money"]>=25 and game_data["lawnmower_counter"]==0 and game_data["scissors_counter"]==1):
-            print(f"You have now ${game_data['money']} in your account before purchase.")
-            game_data["money"]= game_data["money"] -25 
-            game_data["lawnmower_counter"]+=1
-            print(f"You have now ${game_data['money']} in your account after purchase.")
-        elif(game_data["scissors_counter"]==0 and game_data["money"]>=25):
-            print("You do not have scissors")
-        elif(game_data["lawnmower_counter"]==1 and game_data["money"]>=25 and game_data["scissors_counter"]==1):
-            print("You can buy lawnmower one time")
-        else:
-            print("You do not have enought money to buy pair of rusty scissors.")
+            if(game_data["money"]>=25):
+                if(game_data["lawnmower_counter"] < 1):
+                    game_data["money"]= game_data["money"]- 25
+                    game_data["lawnmower_counter"]= 1
+                    print(f"Now You have ${game_data['lawnmower_counter']}lawnmower")
+                    print(f"Now You have ${game_data['money']}in your account")
+                else:
+                    print(f"Now You have one pair of lawnmower.")
+            else:
+                print("You do not have enough money")
+
+########################################   
+##Option 5
+########################################
 
     if(user_input == 5):
-        if(game_data["money"]>=26): 
-            if( game_data["lawnmower_counter"]==1):
-                if(game_data["battery_lawnmower_counter"]==0):
-                   print(f"You have now ${game_data['money']} in your account before purchase.")
-                   game_data["money"]= game_data["money"] -26 
-                   game_data["battery_lawnmower_counter"]+=1
-                else:
-                    print("You have one battery lawnmower")
-            else:
-                print("You do not have any lawnmower")
+        if(game_data["lawnmower_counter"]==1):
+            game_data["money"]+= 50
+            print(f"Congratulations,You have now ${game_data['money']} in your account before purchase.") 
         else:
-            print("You do not have enought money to buy battery lawnmower.")
+            print("You do not have lawnmower, select option[3] to buy the lawnmower ")
 
+########################################   
+##Option 6
+########################################
+  
     if(user_input == 6):
+            if(game_data["money"]>=250):
+                if(game_data["battery_lawnmower_counter"] < 1):
+                    game_data["money"]= game_data["money"]- 250
+                    game_data["battery_lawnmower_counter"]= 1
+                    print(f"Now You have ${game_data['battery_lawnmower_counter']}battery lawnmower")
+                    print(f"Now You have ${game_data['money']}in your account")
+                else:
+                    print(f"Now You have one pair of battery lawnmower.")
+            else:
+                print("You do not have enough money")
+########################################   
+##Option 7
+########################################
+    
+    if(user_input == 7):
         if(game_data["battery_lawnmower_counter"]==1):
-             game_data["money"]= game_data["money"]+ 100
-             print(f"You have now ${game_data['money']} in your account before purchase.")
+            game_data["money"]+= 100
+            print(f"Congratulations,You have now ${game_data['money']} in your account before purchase.") 
+        else:
+            print("You do not have lawnmower, select option[5] to buy the battery ")
 
 
+
+########################################   
+##Option 8
+########################################
+  
+    if(user_input == 8):
+            if(game_data["money"]>=250):
+                if(game_data["battery_lawnmower_counter"] < 1):
+                    game_data["money"]= game_data["money"]- 250
+                    game_data["battery_lawnmower_counter"]= 1
+                    print(f"Now You have ${game_data['battery_lawnmower_counter']}battery lawnmower")
+                    print(f"Now You have ${game_data['money']}in your account")
+                else:
+                    print(f"Now You have one pair of battery lawnmower.")
+            else:
+                print("You do not have enough money")
     
             
 
